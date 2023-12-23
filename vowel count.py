f9aaecfc0046ecb3204974819c0f7af184189146
RabiaArif <151766471+RabiaA-arif@users.noreply.github.com>
2023-12-23 14:30:28 +0500

    Add files via upload

---
+++ vowel count.py
@@ -0,0 +1,12 @@
+x = (input("write alphabet"))
+y = "a e i o u"
+print(x)
+print(y)
+print(x in y,"vowel")
+
+
+c = input("enter character")
+if c=="a" or c=="e" or c=="i" or c=="o" or c=="u":
+    print("character is vowel")
+else:
+    print("consonet")    
